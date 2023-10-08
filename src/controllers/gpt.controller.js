fb33b058338b34b21d175eb1e3749af0fd8e9646
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-10-08 18:01:14 -0300

    refactor: change temperature

--- src/controllers/gpt.controller.js
+++ src/controllers/gpt.controller.js
@@ -3,6 +3,8 @@ const { Configuration, OpenAIApi } = require('openai');
 exports.createDescriptions = async (req, res) => {
     const { description } = req.body;
 
+    //if (!description) res.status(422).json(['Item sem descrição'])
+
     const config = new Configuration({
         apiKey: process.env.USER_GPT_KEY,
         organization: process.env.ORGANZATION_KEY,
@@ -11,17 +13,17 @@ exports.createDescriptions = async (req, res) => {
     const openaiInstance = new OpenAIApi(config);
 
     try {
-        const prompt = `Gere uma descrição para o produto: ${description}`
+        const prompt = `Gere uma descrição eficiente, totalmente SEO-friendly para o produto: ${description}.`
         const completion = await openaiInstance.createCompletion({
             model: 'text-davinci-003',
             prompt: prompt,
-            max_tokens: 2048,
-            temperature: 0.5
+            max_tokens: 2049,
+            temperature: 0.7
         })
 
         const response = completion.data.choices[0].text.trim();
 
-        res.status(201).json({ response })
+        res.status(201).json({ description, response })
     } catch (error) {
         const responseError = {
             status: error.response.status,
