ce308e18784e17d258f5c2b09f66466995a7ac88
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-10-09 16:53:03 -0300

    test: ensure ConfigService get a calleble instance

--- src/tests/config.service.test.ts
+++ src/tests/config.service.test.ts
@@ -10,6 +10,15 @@ describe('env config testing', () => {
     });
 
     it('should test if a enviroment param is string', () => {
-        expect(typeof sut.environment).toBe('string');
+        expect(typeof sut['environment']).toBe('string');
+    });
+
+    it('should test match value is a string value', () => {
+        expect(sut['environment']).toMatch('development');
+    });
+
+    it('should test if ConfigService getInstance', () => {
+        const instance = ConfigService.getInstance(enviroment);
+        expect(instance).toBeInstanceOf(ConfigService);
     });
 });
