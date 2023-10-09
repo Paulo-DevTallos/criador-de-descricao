fa73c1e8b25a7107d0f1b0870a42bd3d8e2c9a4d
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-10-09 20:42:20 -0300

    refactor: create factory to instanciate ConfigService

--- src/tests/config.service.test.ts
+++ src/tests/config.service.test.ts
@@ -1,9 +1,13 @@
 import { ConfigService } from "../shared/services/env-config/config.service";
 
+const makeSut = (enviroment: string) => {
+    const env = enviroment;
+    const sut = ConfigService.getInstance(env);
+    return sut;
+}
+
 describe('env config testing', () => {
-    let sut: ConfigService;
-    const enviroment = 'test';
-    sut = new ConfigService(enviroment);
+    const sut: ConfigService = makeSut('test');
 
     it('should test if ConfigService is defined', () => {
         expect(sut).toBeDefined();
@@ -18,22 +22,17 @@ describe('env config testing', () => {
     });
 
     it('should test if ConfigService getInstance', () => {
-        const instance = ConfigService.getInstance(enviroment);
+        const instance = ConfigService.getInstance('test');
         expect(instance).toBeInstanceOf(ConfigService);
     });
 
     it('should test if getAppPort is calleble using getInstance', () => {
-        const instance = ConfigService.getInstance(enviroment);
+        const instance = ConfigService.getInstance('test');
         expect(instance.getAppPort()).toBe(3036);
     });
 
     it('should test match value for NODE_ENV', () => {
-        const instance = ConfigService.getInstance(enviroment);
+        const instance = ConfigService.getInstance('test');
         expect(instance.getNodeEnv()).toBe('test');
-    });
-
-    it('should return NODE_ENV if is defined', () => {
-        process.env.NODE_ENV = 'production'
-        expect(sut.getNodeEnv()).toBe('production');
     });
 });
