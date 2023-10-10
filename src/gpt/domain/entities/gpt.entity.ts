38be93c4d3f6bf87489a23cf2fe32d5f6270ad65
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-10-09 22:22:31 -0300

    feat: create gpt entity

---
+++ src/gpt/domain/entities/gpt.entity.ts
@@ -0,0 +1,8 @@
+export class GptEntity {
+    constructor(
+        public title: string,
+        public description: string,
+        public created_at: Date,
+        public updated_at: Date,
+    ) {}
+}
--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const gptController = require('../controllers/gpt.controller');
-
-router.post('/create/description', gptController.createDescriptions);
-
-module.exports = router;
